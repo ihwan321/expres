658cfd683d51f82b17dee26836fcafb486e0a856
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-23 01:34:51 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,51 @@
+<?php
+# contoh web 
+# https://coincycles.com/doge/?r=EC-UserId-64162
+# https://joinfaucet.com/doge/?r=EC-UserId-64162
+# dan site sejenis yang timernya 60 detik
+
+
+$ua ="user-agent: Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5 Plus) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36";
+#
+$url ="https://coincycles.com/doge/claim.php?coin=DOGE";
+$phpsessid = "cookie: PHPSESSID=f15eaca01a39b47bdafca3ec32307667";
+$token = "cookie: DOGEToken=3WegPgymOuquyLG3RvrWBY5WKEM7J5ul";
+#
+$url1 ="https://joinfaucet.com/doge/claim.php?coin=DOGE";
+$phpsessid1 = "cookie: PHPSESSID=qlbrvd3rkrnntc4sg97d0jtj12";
+$token1 = "cookie: DOGEToken=7P2iToYnO4uMXlSkatVfbCmfONarmFCC";
+#
+$url2 ="https://coincycles.com/eth/claim.php?coin=ETH";
+$phpsessid2 = "cookie: PHPSESSID=f15eaca01a39b47bdafca3ec32307667";
+$token2 = "cookie: ETHToken=ukbSrMAFqce0JdpdYLFVpVBGqQlKXyR2";
+#
+$url3 ="https://coincycles.com/bch/claim.php?coin=BCH";
+$phpsessid3 = "cookie: PHPSESSID=ff6083b69f9a1a2a053530b234af8778";
+$token3= "cookie: BCHToken=RGH9iuOru5GehVoblkbinwI6FM9AHVqr";
+#
+$url4 ="https://coincycles.com/trx/claim.php?coin=TRX";
+$phpsessid4 = "cookie: PHPSESSID=8ef801e484488d6ad76f147c72aaffbc";
+$token4 = "cookie: TRXToken=HIHF5jy3kVxnlEiBsxuM5mpSJnIB6oLq";
+#
+$url5 ="https://coincycles.com/xrp/claim.php?coin=XRP";
+$phpsessid5 = "cookie: PHPSESSID=a0081ff58d8dec0d68c2b43262c1b445";
+$token5 = "cookie: XRPToken=U60Bvk28WKrTsQGSzFipQIRuxpnNLA6d";
+##
+$url6 ="https://coincycles.com/zec/claim.php?coin=ZEC";
+$phpsessid6 = "cookie: PHPSESSID=ffc0cd5f9f3a9a164c3d51ef76aee9dc";
+$token6 = "cookie: ZECToken=vRklibA19wc2pYAMiVnAkYKM20xT0Dt7";
+##
+$url7 ="https://coincycles.com/exs/claim.php?coin=EXS";
+$phpsessid7 = "cookie: PHPSESSID=bb36b27cedfcc33bc944e50ecd418aa8";
+$token7 = "cookie: EXSToken=4NFZYeh157nL2W4S8yT7Mr1bfl1LWdxk";
+##
+$url8 ="https://coincycles.com/dgb/claim.php?coin=DGB";
+$phpsessid8 = "cookie: PHPSESSID=58f0a05075088166181fd10e36ffeb87";
+$token8 = "cookie: DGBToken=7jDH34uNI5toz9kjW1UlWe6KnU7MP7uA";
+##
+$url9 ="https://coincycles.com/bcn/claim.php?coin=BCN";
+$phpsessid9 = "cookie: PHPSESSID=5f408d5839ea71bf773e16b859582508";
+$token9 = "cookie: BCNToken=Ouq4wzimZwE6QkPaMDseszFbQbv9Qd74";
+
+
+?>
